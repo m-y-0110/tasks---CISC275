455d0309a2080b1a7b0abafede2025b770255bc0
Mantra Yang <myang@udel.edu>
2024-02-18 12:44:36 -0500

    Solved countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -81,7 +81,10 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const fourLetter: string[] = words.filter(
+        (words: string): boolean => words.length < 4
+    );
+    return fourLetter.length;
 }
 
 /**
