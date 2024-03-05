4cc5bdc3024c22ad3bdf933d8b8688b1a6fa66b4
Mantra Yang <myang@udel.edu>
2024-03-05 17:34:34 -0500

    Solved all functions.

--- src/nested.ts
+++ src/nested.ts
@@ -216,40 +216,50 @@ export function changeQuestionTypeById(
     );
 }
 
-// /**
-//  * Consumes an array of Questions and produces a new array of Questions, where all
-//  * the Questions are the same EXCEPT for the one with the given `targetId`. That
-//  * Question should be the same EXCEPT that its `option` array should have a new element.
-//  * If the `targetOptionIndex` is -1, the `newOption` should be added to the end of the list.
-//  * Otherwise, it should *replace* the existing element at the `targetOptionIndex`.
-//  *
-//  * Remember, if a function starts getting too complicated, think about how a helper function
-//  * can make it simpler! Break down complicated tasks into little pieces.
-//  */
-// export function editOption(
-//     questions: Question[],
-//     targetId: number,
-//     targetOptionIndex: number,
-//     newOption: string
-// ): Question[] {
-//     function newList(
-//         options1: string[],
-//         targetOptionIndex: number,
-//         newOption: string
-//     ): string[] {
-//         if (targetOptionIndex === -1) {
-//             return [...options1, newOption];
-//         } else {
-//             options1[targetOptionIndex] = newOption;
-//             return options1;
-//         }
-//     }
-//     return questions.map(
-//         (question: Question): Question =>
-//             question.id === targetId
-//                 ? const newList1 = newList(question.options, targetOptionIndex,newOption);
-//                 {...question, options: newList1}: { ...question });
-// }
+/**
+ * Consumes an array of Questions and produces a new array of Questions, where all
+ * the Questions are the same EXCEPT for the one with the given `targetId`. That
+ * Question should be the same EXCEPT that its `option` array should have a new element.
+ * If the `targetOptionIndex` is -1, the `newOption` should be added to the end of the list.
+ * Otherwise, it should *replace* the existing element at the `targetOptionIndex`.
+ *
+ * Remember, if a function starts getting too complicated, think about how a helper function
+ * can make it simpler! Break down complicated tasks into little pieces.
+ */
+
+function newList(
+    options1: string[],
+    targetOptionIndex: number,
+    newOption: string
+): string[] {
+    if (targetOptionIndex === -1) {
+        return [...options1, newOption];
+    } else {
+        const newOptions1 = [...options1];
+        newOptions1.splice(targetOptionIndex, 1, newOption);
+        return [...newOptions1];
+    }
+}
+export function editOption(
+    questions: Question[],
+    targetId: number,
+    targetOptionIndex: number,
+    newOption: string
+): Question[] {
+    return questions.map(
+        (question: Question): Question =>
+            question.id === targetId
+                ? {
+                      ...question,
+                      options: newList(
+                          question.options,
+                          targetOptionIndex,
+                          newOption
+                      )
+                  }
+                : { ...question }
+    );
+}
 
 /***
  * Consumes an array of questions, and produces a new array based on the original array.
@@ -257,15 +267,25 @@ export function changeQuestionTypeById(
  * the duplicate inserted directly after the original question. Use the `duplicateQuestion`
  * function you defined previously; the `newId` is the parameter to use for the duplicate's ID.
  */
+function duplicateList(
+    questions: Question[],
+    newId: number,
+    question: Question
+): Question[] {
+    const newList = [...questions];
+    const dupeQuestion = duplicateQuestion(newId, question);
+    const newIndex: number = questions.indexOf(question) + 1;
+    newList.splice(newIndex, 0, dupeQuestion);
+    return [...newList];
+}
+
 export function duplicateQuestionInArray(
     questions: Question[],
     targetId: number,
     newId: number
 ): Question[] {
-    return questions.map(
-        (question: Question): Question =>
-            question.id === targetId
-                ? { ...question, ...duplicateQuestion(newId, question) }
-                : { ...question }
+    const targetIdQuestion: Question = questions.find(
+        (question: Question): boolean => question.id === targetId
     );
+    return duplicateList(questions, newId, targetIdQuestion);
 }
