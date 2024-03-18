6387e15e5271f16c8924c7683b3e9a892f995d75
Mantra Yang <myang@udel.edu>
2024-03-17 23:10:54 -0400

    Solved all tasks but ChangeColor.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "pink",
+    "purple",
+    "yellow",
+    "brown"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [color1, setColor] = useState<string>(COLORS[0]);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.value);
+        setColor(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Change Color</h3>
+            <div>
+                You have chosen{" "}
+                <span style={{ backgroundColor: color1 }}>{color1}</span>.
+            </div>
+            {COLORS.map((color: string, index: number) => (
+                <Form.Check
+                    inline
+                    key={index}
+                    type="radio"
+                    name="colors"
+                    onChange={updateColor}
+                    id={`color-check-${color}`}
+                    label={color}
+                    value={color}
+                    checked={color1 === color}
+                    style={{ backgroundColor: color }}
+                />
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: color1,
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            ></div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>(options[0]);
+
+    function updateUserAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userAnswer">
+                <Form.Label>What is the correct answer?</Form.Label>
+                <Form.Select value={userAnswer} onChange={updateUserAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {userAnswer === expectedAnswer ? <div>✔️</div> : <div>❌</div>}
         </div>
     );
 }
